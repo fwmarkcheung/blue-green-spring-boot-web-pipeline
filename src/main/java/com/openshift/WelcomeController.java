b705e45706aed5da3081a831dc9fe845a697c093
Mark Cheung <mcheung@redhat.com>
2019-12-08 23:47:30 -0500

    First commit

---
+++ src/main/java/com/openshift/WelcomeController.java
@@ -0,0 +1,28 @@
+package com.openshift;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+	// inject via application.properties
+	@Value("${welcome.message:test}")
+	private String message = "Hello World";
+
+	@RequestMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", this.message);
+                
+                String myName = System.getenv("MY_DEPLOYMENT_NAME");
+                if (myName != null && myName.indexOf("blue") >= 0)
+                  model.put("currentDeployment","blue");
+                else
+                  model.put("currentDeployment","green");
+		return "index";
+	}
+
+}
